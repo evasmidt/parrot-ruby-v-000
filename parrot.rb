d582326e05ea15e17979a284f00ada9e9762cc51
Eva Smidt <shevangeline@gmail.com>
2018-02-08 22:38:18 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 #In this lab you'll be defining a method called #parrot.
-#The #parrot method should accept an argument of a string and both 
+#The #parrot method should accept an argument of a string and both
 #puts out that string and return the string at the end of the method.
 #The #parrot method should have a default argument of "Squawk!"
 
